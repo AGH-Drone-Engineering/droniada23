80d650456fa077a7a9b230f99234d3af602a3d0f
Carbon225 <carbon225@protonmail.com>
2023-06-01 02:46:12 +0200

    firebase

--- droniada23_drzewko/actors/firebase_actor.py
+++ droniada23_drzewko/actors/firebase_actor.py
@@ -1,9 +1,22 @@
 import ray
-import cv2
 from omegaconf import DictConfig
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+from droniada23_drzewko.data.telemetry import Telemetry
+from droniada23_drzewko.secrets import get_secret_path
 
 
 @ray.remote
 class FirebaseActor:
     def __init__(self, cfg: DictConfig):
-        pass
+        cred = credentials.Certificate(get_secret_path('firebase.json'))
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+
+    def push_telemetry(self, telemetry: Telemetry):
+        self.db.collection('drone-path').add({
+            'timestamp': firestore.SERVER_TIMESTAMP,
+            'location': firestore.GeoPoint(telemetry.lat, telemetry.lon),
+            'altitude': telemetry.alt,
+        })
